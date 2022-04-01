972cdb4aaed586c171f342cf1d84996fd4976953
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-04-01 13:57:54 +0300

    Created: controller Home

---
+++ HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelloMVC.Models;
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Welcome(GuestResponse guestResponse)
+        {
+            return View("Welcome", guestResponse);
+        }
+    }
+}
