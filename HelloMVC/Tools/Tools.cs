68ea113273a41233bcfc18fe46253f695faa71ae
Volodymyr Kyselevych <wilkarich@gmail.com>
2022-04-11 22:17:46 +0300

    Created: tools (getControllerName, reversedControllerName)

---
+++ HelloMVC/Tools/Tools.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloMVC.Helpers
+{
+    public class Tools
+    {
+        static public string GetControllerName(string controllerName)
+        {
+            int posEntryController = controllerName.IndexOf("Controller");
+
+            if (posEntryController == -1) return "";
+
+            return controllerName.Substring(0, posEntryController);
+        }
+
+        static public string GetReversedString(string value)
+        {
+            char[] charArray = value.ToCharArray();
+            Array.Reverse(charArray);
+
+            return new string(charArray);
+        }
+    }
+}
